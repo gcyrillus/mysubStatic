3832656521906357587be749ddea711c02c8eeef
gcyrillus <1832331+gcyrillus@users.noreply.github.com>
2024-03-22 13:37:28 +0100

    Update mySubStatic.php
    regexs update

--- mySubStatic.php
+++ mySubStatic.php
@@ -10,8 +10,11 @@
 		public $subs;
 		# exemple par défaut
 		public $defaultExpert = array(
-				'@li class="static menu@'	=> 'li class="static menu-item mySubStatic'
-	);
+		'@li class="static menu@'			=> 'li class="static menu-item ',
+		'@li class="menu menu-item active@' => 'li class="menu-item active',
+		'@li class="menu has-children"@'    => 'li class="menu-item has-children"',
+		'@menu-item  menu-item@'			=> 'menu-item '
+		);
 		
 		/**
 			* Constructeur de la classe
@@ -105,10 +108,10 @@
 		# modification de la fonction staticList();
 		public function plxShowStaticListBegin() {		
 			echo self::BEGIN_CODE;
-		?>
-		include('<?= PLX_ROOT.'plugins/'.basename(__DIR__)?>/staticList.php');
-		return true;
-		<?php
+			?>
+			include('<?= PLX_ROOT.'plugins/'.basename(__DIR__)?>/staticList.php');
+			return true;
+			<?php
             echo self::END_CODE;
 		}
 		
@@ -126,45 +129,45 @@
 			$cocoon=array();
 			foreach ($this->plxMotor->aStats as $k => $v) {
 				if(
-					$v['group'] !=''  and $v['group'] !='home' 
+				$v['group'] !=''  and $v['group'] !='home' 
 				and
-					($k == substr($this->plxMotor->cible,0,3) or $v['group'] == $this->plxMotor->aStats[$this->plxMotor->cible]['group'])  
+				($k == substr($this->plxMotor->cible,0,3) or $v['group'] == $this->plxMotor->aStats[$this->plxMotor->cible]['group'])  
 				) {
 					if ($group_active == ''  and $this->staticId() == intval($k) and $v['group'] != '') {
-					$active = plxUtils::strCheck($v['name']);
+						$active = plxUtils::strCheck($v['name']);
 						$cocoon[plxUtils::strCheck($v['name'])]= '<a class="active">'.$active.'</a>';
 						$rel='<?= L_PAGINATION_NEXT_TITLE  ?>: ';
 						continue;// on passe la page courante
 					}
 					#recup URL
 					if ($v['url'][0] == '?') # url interne commençant par ?
-						$url = $this->plxMotor->urlRewrite($v['url']);
+					$url = $this->plxMotor->urlRewrite($v['url']);
 					elseif (plxUtils::checkSite($v['url'], false)) # url externe en http ou autre
-						$url = $v['url'];
+					$url = $v['url'];
 					else # url page statique
-						$url = $this->plxMotor->urlRewrite('?static' . intval($k) . '/' . $v['url']);
-						
+					$url = $this->plxMotor->urlRewrite('?static' . intval($k) . '/' . $v['url']);
+					
 					$cocoon[plxUtils::strCheck($v['name'])]= '<a href="'.$url.'" rel="'.$rel.'" title="'.$rel.' '.plxUtils::strCheck($v['name']).'">'.plxUtils::strCheck($v['name']).'</a>';
 				}		
 			}
-
+			
 			if(<?= $ariane ?> == 1  and $this->plxMotor->aStats[$this->plxMotor->cible]['group'] !='' ) {			
 				#fil d'ariane
 				$breacrumbs='
 				<ul class="repertory menu breadcrumb">
 					<li><a href="'.$this->plxMotor->urlRewrite().'">'.$this->getLang('HOME').'</a></li>'.PHP_EOL;
-				if(array_key_exists(trim(substr($this->plxMotor->aStats[$this->plxMotor->cible]['group'],0,3)),$this->plxMotor->aStats)) {
-				$breacrumbs.= '					<li><a href="'.$this->plxMotor->urlRewrite('?static' . intval(substr($this->plxMotor->aStats[$this->plxMotor->cible]['group'],0,3)) . '/'.$this->plxMotor->aStats[substr($this->plxMotor->aStats[$this->plxMotor->cible]['group'],0,3)]['url']).'">'.$this->plxMotor->aStats[substr($this->plxMotor->aStats[$this->plxMotor->cible]['group'],0,3)]['group'].'</a></li>
+					if(array_key_exists(trim(substr($this->plxMotor->aStats[$this->plxMotor->cible]['group'],0,3)),$this->plxMotor->aStats)) {
+					$breacrumbs.= '					<li><a href="'.$this->plxMotor->urlRewrite('?static' . intval(substr($this->plxMotor->aStats[$this->plxMotor->cible]['group'],0,3)) . '/'.$this->plxMotor->aStats[substr($this->plxMotor->aStats[$this->plxMotor->cible]['group'],0,3)]['url']).'">'.$this->plxMotor->aStats[substr($this->plxMotor->aStats[$this->plxMotor->cible]['group'],0,3)]['group'].'</a></li>
 					<li>'.substr($this->plxMotor->aStats[$this->plxMotor->cible]['group'],3).'</li>'.PHP_EOL;
-				}
-				else {
-				$breacrumbs.='					<li>'.$this->plxMotor->aStats[$this->plxMotor->cible]['group'].'</li>'.PHP_EOL;
-				}
-				$breacrumbs.='					<li><em class="active">'.$active.'</em></li>'.PHP_EOL;			
+					}
+					else {
+					$breacrumbs.='					<li>'.$this->plxMotor->aStats[$this->plxMotor->cible]['group'].'</li>'.PHP_EOL;
+					}
+					$breacrumbs.='					<li><em class="active">'.$active.'</em></li>'.PHP_EOL;			
 				$breacrumbs.='				</ul>';
 			}		
 			if(<?= $navgroup ?> == 1 ) {				
-			#navigation niveau groupe
+				#navigation niveau groupe
 				if(count($cocoon)>1) {				
 					if(array_key_exists(trim(substr($this->plxMotor->aStats[$this->plxMotor->cible]['group'],0,3)),$this->plxMotor->aStats)) {
 						$groupName= substr($this->plxMotor->aStats[$this->plxMotor->cible]['group'],3);
@@ -172,104 +175,89 @@
 					else {
 						$groupName=$this->plxMotor->aStats[$this->plxMotor->cible]['group'];
 					}				
-				$nav = '<nav id="<?= __CLASS__ ?>" class="prevNext"><b style="text-transform:uppercase">'.$groupName.': </b>';
-					foreach ($cocoon as $adj) {
-					$nav .=  $adj;
-					}
-				$nav .=  '</nav>';
-				}
+					$nav = '<nav id="<?= __CLASS__ ?>" class="prevNext"><b style="text-transform:uppercase">'.$groupName.': </b>';
+						foreach ($cocoon as $adj) {
+							$nav .=  $adj;
+						}
+					$nav .=  '</nav>';
+			}
 			}
 			
-		# On va verifier que la page a inclure est lisible
-		if ($this->plxMotor->aStats[$this->plxMotor->cible]['readable'] == 1) {
-			# On genere le nom du fichier a inclure
-			$file = PLX_ROOT . $this->plxMotor->aConf['racine_statiques'] . $this->plxMotor->cible;
-			$file .= '.' . $this->plxMotor->aStats[$this->plxMotor->cible]['url'] . '.php';
-			# Inclusion du fichier
-			ob_start();
-			require $file;
-			$output = ob_get_clean();
-			eval($this->plxMotor->plxPlugins->callHook('plxShowStaticContent'));
-
-			$output= $breacrumbs.PHP_EOL.$output.PHP_EOL.$nav;
-			echo $output;
-		} else {
-			echo $breacrumbs.'<p class="alert orange">' . L_STATICCONTENT_INPROCESS . '</p>'.$nav;
-		}			
-		
-		# fin de traitement de la page statique
-		return true;
+			# On va verifier que la page a inclure est lisible
+			if ($this->plxMotor->aStats[$this->plxMotor->cible]['readable'] == 1) {
+				# On genere le nom du fichier a inclure
+				$file = PLX_ROOT . $this->plxMotor->aConf['racine_statiques'] . $this->plxMotor->cible;
+				$file .= '.' . $this->plxMotor->aStats[$this->plxMotor->cible]['url'] . '.php';
+				# Inclusion du fichier
+				ob_start();
+				require $file;
+				$output = ob_get_clean();
+				eval($this->plxMotor->plxPlugins->callHook('plxShowStaticContent'));
+				
+				$output= $breacrumbs.PHP_EOL.$output.PHP_EOL.$nav;
+				echo $output;
+			} else {
+				echo $breacrumbs.'<p class="alert orange">' . L_STATICCONTENT_INPROCESS . '</p>'.$nav;
+			}			
+			
+			# fin de traitement de la page statique
+			return true;
 			<?php
             echo self::END_CODE;
-	
+			
 		}
 		
 		
 		# reinjection des sous statiques
 		public function ThemeEndBody() {
-		   if(count($this->subs)>0) {
-			foreach($this->subs as $sub => $li) {
-				$html='';
-				$name=$sub;
-				if(is_array($li)) {
-					foreach($li as $k => $val) { 
-						$html .= $k.'			<ul class="sub-menu">'.PHP_EOL;				
-						if(is_array($val)) {
-							foreach($val as $sub => $v) {
-								$v = str_replace("'", "&rsquo;", $v);
-								$html .= $v;
+			if(count($this->subs)>0) {
+				foreach($this->subs as $sub => $li) {
+					$html='';
+					$name=$sub;
+					if(is_array($li)) {
+						foreach($li as $k => $val) { 
+							$html .= $k.'			<ul class="sub-menu">'.PHP_EOL;				
+							if(is_array($val)) {
+								foreach($val as $sub => $v) {
+									$v = str_replace("'", "&rsquo;", $v);
+									$html .= $v;
+								}
 							}
+							$html .='			</ul>'.PHP_EOL;
 						}
-						$html .='			</ul>'.PHP_EOL;
-					}
-				}			  
-				$pregArray=json_encode($this->defaultExpert);
-				$reformat=array(
-				'@{@'	=>	'(',
-				'@}@'=>	')'.PHP_EOL,
-				'@:@'	=>	'=>'
-				);
-				$pregArrayjs = preg_replace(array_keys( $reformat ), array_values( $reformat ), $pregArray);
-
+					}			  
+					$pregArray=json_encode($this->defaultExpert);
+					$reformat=array(
+					'@{@'	=>	'(',
+					'@}@'=>	')'.PHP_EOL,
+					'@:@'	=>	'=>'
+					);
+					$pregArrayjs = preg_replace(array_keys( $reformat ), array_values( $reformat ), $pregArray);
+					
 					echo self::BEGIN_CODE;
 				?>	
-				$output = str_replace('</li><!-- <?= $name ?> -->', ob_get_clean().PHP_EOL.'<?= $html ?>		</li>'.PHP_EOL, $output);
-				<?php
+			$output = str_replace('</li><!-- <?= $name ?> -->', ob_get_clean().PHP_EOL.'<?= $html ?>		</li>'.PHP_EOL, $output);
+			<?php
 				echo self::END_CODE;
 			}			  
 			# nettoyage final
 			echo self::BEGIN_CODE;
 			?>	
-			$replace= <?= 'array'.$pregArrayjs  ?>;
-			$output = preg_replace(array_keys( $replace ), array_values( $replace ), $output);
-			$output = str_replace('</body>', ob_get_clean().'<script src="'.PLX_ROOT.'plugins/<?= __CLASS__ ?>/js/<?= __CLASS__ ?>.js"></script>'.PHP_EOL.'</body>', $output);
+				$replace= <?= 'array'.$pregArrayjs  ?>;
+				$output = preg_replace(array_keys( $replace ), array_values( $replace ), $output);
+				$output = str_replace('</body>', ob_get_clean().'<script src="'.PLX_ROOT.'plugins/<?= __CLASS__ ?>/js/<?= __CLASS__ ?>.js"></script>'.PHP_EOL.'</body>', $output);
 			<?php
-				echo self::END_CODE;
-		   }
+			echo self::END_CODE;
 		}
-		
+	}
+
 	public function AdminStaticsPrepend() {		
 		echo self::BEGIN_CODE;
 		?>
-		$plgPlugin = $plxAdmin->plxPlugins->aPlugins['<?= __CLASS__ ?>'];
-		include PLX_PLUGINS . '<?= __CLASS__ ?>/statiques.php';
-		exit;
-<?php
-		echo self::END_CODE;
-	}
-
-	 /**
-	 * Méthode qui fait la mise a jour du renvoi vers la page plg_article.php au lieu de article.php si le plugin est actif
-	 *
-	 * @author	bazooka07
-	 **/
-	public function AdminArticlePrepend() {
-		echo self::BEGIN_CODE;
-?>
-		$plgPlugin = $plxAdmin->plxPlugins->aPlugins['<?= __CLASS__ ?>'];
-		include PLX_PLUGINS . '<?= __CLASS__ ?>/plg_article.php';
-		exit;
-<?php
+			$plgPlugin = $plxAdmin->plxPlugins->aPlugins['<?= __CLASS__ ?>'];
+			include PLX_PLUGINS . '<?= __CLASS__ ?>/statiques.php';
+			exit;
+		<?php
 		echo self::END_CODE;
 	}
 }
